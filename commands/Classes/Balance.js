55c43d09cef685666b979b9e615bcf2c12532fd5
João Vítor <joaovitorsf7@hotmail.com>
2021-05-30 09:01:47 -0300

    refatoração e fix no rndnote.

--- commands/Classes/Balance.js
+++ commands/Classes/Balance.js
@@ -1,31 +1,72 @@
 const Discord = require("discord.js");
 const ms = require("parse-ms");
 
+function getDocUser(database, message, userinfo) {
+  return database
+    .collection("Usuarios")
+    .doc(message.guild.id)
+    .collection("Usuarios")
+    .where("id", "==", userinfo.id)
+    .get();
+}
+
+function getData(database, message) {
+  return getDocUser(database, message, message.author).then(function (
+    querySnapshot
+  ) {
+    let profile;
+    querySnapshot.forEach(function (documentSnapshot) {
+      profile = documentSnapshot.data();
+    });
+    return profile;
+  });
+}
+
+function addBonus(database, message, type) {
+  let qtde = Math.floor(Math.random() * 50) + 1;
+  message.channel.send(
+    new Discord.MessageEmbed()
+      .setColor("#0099ff")
+      .addField(`Moedas do ${type}`, qtde + " moedas.")
+  );
+  database
+    .collection("Usuarios")
+    .doc(message.guild.id)
+    .collection("Usuarios")
+    .where("id", "==", message.author.id)
+    .get()
+    .then(function (querySnapshot) {
+      querySnapshot.forEach(function (documentSnapshot) {
+        database
+          .collection("Usuarios")
+          .doc(message.guild.id)
+          .collection("Usuarios")
+          .doc(documentSnapshot.id)
+          .update({ money: documentSnapshot.data().money + qtde });
+      });
+    });
+  database
+    .collection("Usuarios")
+    .doc(message.guild.id)
+    .collection("Usuarios")
+    .where("id", "==", message.author.id)
+    .get()
+    .then(function (querySnapshot) {
+      querySnapshot.forEach(function (documentSnapshot) {
+        database
+          .collection("Usuarios")
+          .doc(message.guild.id)
+          .collection("Usuarios")
+          .doc(documentSnapshot.id)
+          .update({ daily: Date.now() });
+      });
+    });
+}
+
 class Balance {
   async daily(database, message) {
     const timeout = 86400000; // 24 horas em milisegundos
-    let qtde = Math.floor(Math.random() * 50) + 1;
-
-    function getDocUser(userinfo) {
-      return database
-        .collection("Usuarios")
-        .doc(message.guild.id)
-        .collection("Usuarios")
-        .where("id", "==", userinfo.id)
-        .get();
-    }
-
-    function getData() {
-      return getDocUser(message.author).then(function (querySnapshot) {
-        let profile;
-        querySnapshot.forEach(function (documentSnapshot) {
-          profile = documentSnapshot.data();
-        });
-        return profile;
-      });
-    }
-
-    let data = await getData();
+    let data = await getData(database, message);
     let daily = data.daily;
 
     let dailyAux = daily;
@@ -35,70 +76,13 @@ class Balance {
         `você já pegou suas moedas do dia.\nVocê precisa esperar ${time.hours}h ${time.minutes}m ${time.seconds}s para poder pegar novamente.`
       );
     } else {
-      message.channel.send(
-        new Discord.MessageEmbed()
-          .setColor("#0099ff")
-          .addField("Moedas do Dia", qtde + " moedas.")
-      );
-      database
-        .collection("Usuarios")
-        .doc(message.guild.id)
-        .collection("Usuarios")
-        .where("id", "==", message.author.id)
-        .get()
-        .then(function (querySnapshot) {
-          querySnapshot.forEach(function (documentSnapshot) {
-            database
-              .collection("Usuarios")
-              .doc(message.guild.id)
-              .collection("Usuarios")
-              .doc(documentSnapshot.id)
-              .update({ money: documentSnapshot.data().money + qtde });
-          });
-        });
-      database
-        .collection("Usuarios")
-        .doc(message.guild.id)
-        .collection("Usuarios")
-        .where("id", "==", message.author.id)
-        .get()
-        .then(function (querySnapshot) {
-          querySnapshot.forEach(function (documentSnapshot) {
-            database
-              .collection("Usuarios")
-              .doc(message.guild.id)
-              .collection("Usuarios")
-              .doc(documentSnapshot.id)
-              .update({ daily: Date.now() });
-          });
-        });
+      addBonus(database, message, "Dia");
     }
   }
 
   async weekly(database, message) {
     const timeout = 604800000; // 7 dias em milisegundos
-    let qtde = Math.floor(Math.random() * 50) + 1;
-
-    function getDocUser(userinfo) {
-      return database
-        .collection("Usuarios")
-        .doc(message.guild.id)
-        .collection("Usuarios")
-        .where("id", "==", userinfo.id)
-        .get();
-    }
-
-    function getData() {
-      return getDocUser(message.author).then(function (querySnapshot) {
-        let profile;
-        querySnapshot.forEach(function (documentSnapshot) {
-          profile = documentSnapshot.data();
-        });
-        return profile;
-      });
-    }
-
-    let data = await getData();
+    let data = await getData(database, message);
     let weekly = data.weekly;
 
     let weeklyAux = weekly;
@@ -108,70 +92,13 @@ class Balance {
         `você já pegou suas moedas da semana.\nVocê precisa esperar ${time.days}d ${time.hours}h ${time.minutes}m ${time.seconds}s para poder pegar novamente.`
       );
     } else {
-      message.channel.send(
-        new Discord.MessageEmbed()
-          .setColor("#0099ff")
-          .addField("Moedas da Semana", qtde + " moedas.")
-      );
-      database
-        .collection("Usuarios")
-        .doc(message.guild.id)
-        .collection("Usuarios")
-        .where("id", "==", message.author.id)
-        .get()
-        .then(function (querySnapshot) {
-          querySnapshot.forEach(function (documentSnapshot) {
-            database
-              .collection("Usuarios")
-              .doc(message.guild.id)
-              .collection("Usuarios")
-              .doc(documentSnapshot.id)
-              .update({ money: documentSnapshot.data().money + qtde });
-          });
-        });
-      database
-        .collection("Usuarios")
-        .doc(message.guild.id)
-        .collection("Usuarios")
-        .where("id", "==", message.author.id)
-        .get()
-        .then(function (querySnapshot) {
-          querySnapshot.forEach(function (documentSnapshot) {
-            database
-              .collection("Usuarios")
-              .doc(message.guild.id)
-              .collection("Usuarios")
-              .doc(documentSnapshot.id)
-              .update({ weekly: Date.now() });
-          });
-        });
+      addBonus(database, message, "Semana");
     }
   }
 
   async monthly(database, message) {
     const timeout = 2592000000; // 30 dias em milisegundos
-    let qtde = Math.floor(Math.random() * 50) + 1;
-
-    function getDocUser(userinfo) {
-      return database
-        .collection("Usuarios")
-        .doc(message.guild.id)
-        .collection("Usuarios")
-        .where("id", "==", userinfo.id)
-        .get();
-    }
-
-    function getData() {
-      return getDocUser(message.author).then(function (querySnapshot) {
-        let profile;
-        querySnapshot.forEach(function (documentSnapshot) {
-          profile = documentSnapshot.data();
-        });
-        return profile;
-      });
-    }
-
-    let data = await getData();
+    let data = await getData(database, message);
     let monthly = data.monthly;
 
     let monthlyAux = monthly;
@@ -181,43 +108,7 @@ class Balance {
         `você já pegou suas moedas do mês.\nVocê precisa esperar ${time.days}d ${time.hours}h ${time.minutes}m ${time.seconds}s para poder pegar novamente.`
       );
     } else {
-      message.channel.send(
-        new Discord.MessageEmbed()
-          .setColor("#0099ff")
-          .addField("Moedas do Mês", qtde + " moedas.")
-      );
-      database
-        .collection("Usuarios")
-        .doc(message.guild.id)
-        .collection("Usuarios")
-        .where("id", "==", message.author.id)
-        .get()
-        .then(function (querySnapshot) {
-          querySnapshot.forEach(function (documentSnapshot) {
-            database
-              .collection("Usuarios")
-              .doc(message.guild.id)
-              .collection("Usuarios")
-              .doc(documentSnapshot.id)
-              .update({ money: documentSnapshot.data().money + qtde });
-          });
-        });
-      database
-        .collection("Usuarios")
-        .doc(message.guild.id)
-        .collection("Usuarios")
-        .where("id", "==", message.author.id)
-        .get()
-        .then(function (querySnapshot) {
-          querySnapshot.forEach(function (documentSnapshot) {
-            database
-              .collection("Usuarios")
-              .doc(message.guild.id)
-              .collection("Usuarios")
-              .doc(documentSnapshot.id)
-              .update({ monthly: Date.now() });
-          });
-        });
+      addBonus(database, message, "Mês");
     }
   }
 }
