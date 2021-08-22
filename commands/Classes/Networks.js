79fb68508bb2b7a662a15284e077405ddb6af194
João Vítor <joaovitorsf7@hotmail.com>
2021-08-22 11:47:47 -0300

    refatoração e remoção do discord-blackjack.

--- commands/Classes/Networks.js
+++ commands/Classes/Networks.js
@@ -1,12 +1,10 @@
 class Networks {
   discord(message) {
-    message.channel.send("https://discord.gg/Aju3mdSF3R");
+    message.channel.send("https://discord.gg/zz2MSDWk9a");
   }
 
   github(message) {
-    message.channel.send(
-      "https://github.com/jjoaovitor7\nhttps://github.com/jjoaovitor7-unit"
-    );
+    message.channel.send("https://github.com/jjoaovitor7/jjao-bot");
   }
 
   kofi(message) {
