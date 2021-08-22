16eda39ecd557a81da9ed65e2bdde383a3627cc2
João Vítor <joaovitorsf7@hotmail.com>
2021-08-22 11:47:47 -0300

    refatoração e remoção do discord-blackjack.

--- modules/showServers.js
+++
@@ -1,8 +0,0 @@
-function showServers(client) {
-  // servidores em que JJao está
-  client.guilds.cache.forEach((guild) => {
-    console.log(`${guild.id} - ${guild.name}`);
-  });
-}
-
-module.exports = showServers;
