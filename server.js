4c84643ba2045ee53d6d5ae94ce269fb6c7ffe5f
João Vítor <48962164+jjoaovitor7@users.noreply.github.com>
2024-06-12 18:11:34 -0300

    feat: alteração na mensagem de "Servidor web rodando em...".

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const path = require("path");
+const os = require("os");
 
 module.exports = function () {
   const app = express()
   const port = process.env.PORT || 3000;
+  const networkInterfaces = os.networkInterfaces();
+  const networkInterfaceZero = networkInterfaces[Object.keys(networkInterfaces)[0]];
+  const ipv4Address = networkInterfaceZero.find(interface => interface.family === 'IPv4').address;
 
   app.use(express.static(path.join(__dirname, "public")));
 
@@ -12,6 +16,6 @@ module.exports = function () {
   });
 
   app.listen(port, () => {
-    console.log(`Servidor web rodando em: ${port}`)
+    console.log(`Servidor web rodando em "${ipv4Address}:${port}".`)
   })
 }
